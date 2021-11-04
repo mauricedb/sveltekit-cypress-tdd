d2e54408fa050160f6d3eca556c63f96e1174df6
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-11-04 10:35:55 +0100

    Prebuild header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { page } from '$app/stores';
+
+	const navLinks = [
+		{ title: 'Home', path: '/' },
+		{ title: 'Movies', path: '/movies' },
+		{ title: 'About', path: '/about' }
+	];
+</script>
+
+<header>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="/">SvelteKit &amp; Cypress</a>
+			<div class="collapse navbar-collapse">
+				<ul class="navbar-nav">
+					{#each navLinks as link}
+						<li class="nav-item">
+							<a href={link.path} class="nav-link" class:active={$page.path === link.path}>
+								{link.title}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
