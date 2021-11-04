23211492a702609064b353d5df768227ce14efb8
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-11-04 14:28:35 +0100

    Steps complete

---
+++ src/lib/MovieCard.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let movie;
+</script>
+
+<div class="card m-2 shadow">
+	<img
+		src={`//image.tmdb.org/t/p/w300${movie.backdrop_path}`}
+		class="card-img-top"
+		alt={movie.title}
+	/>
+	<div class="card-body">
+		<h5 class="card-title">{movie.title}</h5>
+		<p class="card-text">
+			{movie.overview}
+		</p>
+	</div>
+</div>
+
+<style>
+	.card {
+		flex-basis: 18rem !important;
+		max-width: 18rem;
+	}
+</style>
